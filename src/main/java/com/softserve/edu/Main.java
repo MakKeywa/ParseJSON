8bb52d972ec5c68bb7c9f1943477ab5e8d90041a
MakKeywa <mak.keywa@gmail.com>
2021-12-13 19:15:10 +0200

    added method to read from file and clean all whitespaces

--- src/main/java/com/softserve/edu/Main.java
+++ src/main/java/com/softserve/edu/Main.java
@@ -1,4 +1,13 @@
 package com.softserve.edu;
 
 public class Main {
+
+    private static final Utils utils = new Utils();
+    private static final String FILE_PATH = "src/main/resources/settings.json";
+
+    public static void main(String[] args){
+        String inputJson = utils.readFile(FILE_PATH);
+        inputJson = utils.cleanJson(inputJson);
+        System.out.println(inputJson);
+    }
 }
--- src/main/java/com/softserve/edu/Utils.java
+++ src/main/java/com/softserve/edu/Utils.java
@@ -1,4 +1,29 @@
 package com.softserve.edu;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Utils {
+
+    public String readFile(String filename) {
+        StringBuilder allText = new StringBuilder("");
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                allText.append(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("No such file!");
+            e.printStackTrace();
+        }
+        return allText.toString();
+    }
+
+    public String cleanJson(String input){
+        return input.replaceAll("\\s+","");
+    }
 }
